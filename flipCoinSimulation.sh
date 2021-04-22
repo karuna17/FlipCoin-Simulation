0464f49cc40c507ed3479d739f39ffc8d3c025bf
karuna <karunakakade8@gmail.com>
2021-04-22 22:53:13 +0530

    Added: Win Heads or Tails and by how much

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,20 +1,31 @@
 #!/bin/bash -x
 
-flipCoin=$((RANDOM%2))
-count=0
+count=1
 h=0
 t=0
-for ((i=1; i<=10; i++))
+
+for ((i=1; i<=50; i++))
 do
-  ((count++))
-if [ $flipCoin -eq 1 ]
-then 
-  echo HEADS
-  ((h++))
-else
-  echo TAILS
+  ((coun++))
+
+if [ $((RANDOM%2)) -eq 0 ]
+  then 
+   ((h++))
+elif [ $((RANDOM%2)) -eq 1 ]
+  then
   ((t++))
 fi
 done
-echo "Heads Won: " $h
-echo "Tails Won: " $t
+
+echo "Heads : " $h
+echo "Tails : " $t
+
+if [ $h -gt $t ]
+then 
+ echo Heads Won
+elif [ $h -lt $t ]
+then 
+ echo Tails Won
+else
+ echo Tie between Heads and Tails
+fi
