dfe33a3ab4df8e7ee1c906dcfa178d5d66cf4fde
karuna <karunakakade8@gmail.com>
2021-04-22 20:18:46 +0530

    Added: Count of won Head or Tails

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,11 +1,20 @@
 #!/bin/bash -x
 
 flipCoin=$((RANDOM%2))
-
+count=0
+h=0
+t=0
+for ((i=1; i<=10; i++))
+do
+  ((count++))
 if [ $flipCoin -eq 1 ]
 then 
   echo HEADS
+  ((h++))
 else
   echo TAILS
+  ((t++))
 fi
-
+done
+echo "Heads Won: " $h
+echo "Tails Won: " $t
